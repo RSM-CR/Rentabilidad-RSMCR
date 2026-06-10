bd636a2b9814569e19cf51b1ebafb38f3d789304
CorredorRSM <rcorredor@rsm.cr>
2026-06-10 14:44:28 -0600

    Prueba de servidor local backend corriendo

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const http = require('node:http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end('¡Hola Mundo!\n');
 });
 
 server.listen(port, () => {
