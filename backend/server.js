9ea3aa070b3ef8e8883b85da10a6e1eb4f7f15d3
Jan Rodríguez Gómez <jrodriguez@rsm.cr>
2026-06-11 10:36:12 -0600

    Configuraciones extras para dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+//definir el puerto y la URL de origen permitida para CORS desde las variables de entorno
 const port = process.env.PORT || 3000;
+const allowedOrigin = process.env.CORS_ORIGIN;
 
 //cargar las variables de entorno desde el archivo .env
 require('dotenv').config();
 
-app.use(cors());
+//configurar CORS para permitir solicitudes desde la URL de origen permitida
+app.use(cors({
+    origin: allowedOrigin
+}));
 
 //definir una ruta para probar el servidor
 app.get('/', (req, res) => {
@@ -18,3 +24,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://:${port}/`);
 })
+
+
+
