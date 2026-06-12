b2cb351547c4de7ab83834d9640dace79366f7a0
Mariangel Monge <mmonge@rsm.cr>
2026-06-12 17:36:43 -0600

    Login frontend terminado

--- rentabilidad/src/Components/LoginSignup/LoginSignup.jsx
+++ rentabilidad/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,31 +1,62 @@
-import React from 'react'
-import './LoginSignup.css'
-import logo_icon from '../LoginSignup/assets/logo.webp'
-import user_icon from '../LoginSignup/assets/user.png'
-import password_logo from '../LoginSignup/assets/password.png'
+import React, { useState } from "react";
+import "./LoginSignup.css";
+import logo_icon from "../LoginSignup/assets/logo.webp";
+import user_icon from "../LoginSignup/assets/user.png";
+import password_logo from "../LoginSignup/assets/password.png";
 
 export const LoginSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
-    <div className='container'>
-        <div className='header'>
-            <img className='logo' src={logo_icon} alt="" />
-            <div className='text'>Inicio de Sesión</div>
+    <div className="container">
+      <div className="header">
+        <img className="logo" src={logo_icon} alt="logo" />
+        <div className="text">Inicio de Sesión</div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="user icon" />
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className='inputs'>
-            <div className='input'>
-                <img src={user_icon} alt="" />
-                <input type="email" placeholder='Email'/>
-            </div>
-            <div className='input'>
-                <img src={password_logo} alt="" />
-                <input type="password" placeholder='Contraseña' />
-            </div>
+
+        <div className="input">
+          <img src={password_logo} alt="password icon" />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <div className="forgot-password">¿Olvidaste tu contraseña? <span>Haz click acá</span></div>
-        <div className="submit-container">
-            <div className="submit">Iniciar Sesión</div>
-            <div className="submit">Crear Cuenta</div>
+
+        <div className="buttons">
+          <div className="stay-login">
+            <label>
+              <input type="checkbox" /> Mantener sesión iniciada
+            </label>
+          </div>
+
+          <button className="submit" onClick={handleSubmit}>
+            Ingresar
+          </button>
+
+          <button className="new-acc">
+            Crear una cuenta nueva
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
